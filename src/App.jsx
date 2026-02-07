df096eccfb20700eb8cca1719d0d13e34188550c
3boku <a24746440@gmail.com>
2026-02-07 22:08:01 +0900

    feat: yes누르면 들어와지도록

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [answer, setAnswer] = useState(null) // 'yes' | 'no' | null
   const cardRef = useRef(null)
 
-  const handleNo = () => {
+  const handleNo = (e) => {
+    // 마우스 올리면 도망
     const card = cardRef.current
     if (!card) return
     const cardRect = card.getBoundingClientRect()
@@ -23,6 +25,17 @@ function App() {
   const [escaped, setEscaped] = useState(false)
   const [noPos, setNoPos] = useState({ x: 0, y: 0 })
 
+  if (answer === 'yes') {
+    return (
+      <div className="bg-white w-200 h-150 rounded-3xl shadow-2xl flex flex-col items-center justify-center gap-8 p-10">
+        <h1 className="text-pink-500 text-3xl font-bold">감사해요🤩</h1>
+        <img src={"https://media1.tenor.com/m/RiZpodi6JD0AAAAC/fast-cat-cat-excited.gif"} alt="yippee" className="w-50 h-50" />
+      </div>
+    )
+  }
+
+  
+
   return (
     <>
       <div ref={cardRef} className="bg-white w-200 h-150 rounded-3xl shadow-2xl flex flex-col items-center justify-center gap-8 p-10 relative overflow-hidden">
@@ -31,7 +44,10 @@ function App() {
         </h1>
 
         <div className="flex flex-row items-center justify-center gap-50">
-          <button className="bg-pink-500 hover:bg-pink-600 text-white text-lg font-semibold px-8 py-3 rounded-full shadow-md hover:shadow-lg transition-all duration-200 cursor-pointer border-none">
+          <button
+            className="bg-pink-500 hover:bg-pink-600 text-white text-lg font-semibold px-8 py-3 rounded-full shadow-md hover:shadow-lg transition-all duration-200 cursor-pointer border-none"
+            onClick={() => setAnswer('yes')}
+          >
             YES
           </button>
           {escaped && (
@@ -41,7 +57,6 @@ function App() {
             className="bg-gray-300 text-white text-lg font-semibold px-8 py-3 rounded-full shadow-md hover:shadow-lg cursor-pointer border-none transition-all duration-300"
             style={escaped ? { position: 'absolute', left: noPos.x, top: noPos.y } : {}}
             onMouseEnter={handleNo}
-            onClick={handleNo}
           >
             NO
           </button>
