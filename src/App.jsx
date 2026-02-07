48c6830b41fcdb4a0588af794a8ac3280b983757
3boku <a24746440@gmail.com>
2026-02-07 21:57:22 +0900

    feat: 기본 카드 디자인

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,26 +6,20 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="bg-white w-200 h-150 rounded-3xl shadow-2xl flex flex-col items-center justify-center gap-8 p-10">
+        <h1 className="text-3xl font-bold text-pink-500 drop-shadow-sm">
+          나랑 발렌타인데이 보낼래? 😋
+        </h1>
+
+        <div className="flex flex-row items-center justify-center gap-50">
+          <button className="bg-pink-500 hover:bg-pink-600 text-white text-lg font-semibold px-8 py-3 rounded-full shadow-md hover:shadow-lg transition-all duration-200 cursor-pointer border-none">
+            YES
+          </button>
+          <button className="bg-gray-300 text-white text-lg font-semibold px-8 py-3 rounded-full shadow-md hover:shadow-lg transition-all duration-200 cursor-pointer border-none">
+            NO
+          </button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
